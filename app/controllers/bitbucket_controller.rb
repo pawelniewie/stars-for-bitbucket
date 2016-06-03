e07ad203df2376c9d4fa38772289ed632bbb9f17
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-03 14:36:20 +0200

    added oauthConsumer to the descriptor, storing keys using secrets

--- app/controllers/bitbucket_controller.rb
+++ app/controllers/bitbucket_controller.rb
@@ -3,7 +3,8 @@ class BitbucketController < ApplicationController
 	def descriptor
 		render :descriptor, locals: { 
 			base_url: 'https://' + ApplicationController.renderer.defaults[:http_host],
-			plugin_key: PluginKeyService::PLUGIN_KEY
+			plugin_key: PluginKeyService::PLUGIN_KEY,
+      client_id: Rails.application.secrets.client_id
 		}
 	end
 
