3ed7205bbb4f48b09f36fd684127821387256cde
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-03 20:08:09 +0200

    added HTTP_HOST

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 ApplicationController.renderer.defaults.merge!(
-  http_host: '608611dd.ngrok.io',
+  http_host: Rails.application.secrets.base_url,
   https: true
 )
