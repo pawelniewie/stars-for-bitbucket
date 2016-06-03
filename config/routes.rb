a2396fdb2385b53016d9d18005756b4ead424826
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-03 20:01:00 +0200

    default format for descriptor set correctly to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/bitbucket/descriptor', to: 'bitbucket#descriptor', format: :json 
+  get '/bitbucket/descriptor', to: 'bitbucket#descriptor', defaults: { format: :json }
   post '/installed', to: 'lifecycle#installed'
   post '/uninstalled', to: 'lifecycle#uninstalled'
   get '/stars', to: 'stars#show'
