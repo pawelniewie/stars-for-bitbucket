75fc8e877e85b839308982d65603f963ed96bbca
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-04 07:58:52 +0200

    name of the plugin is dynamic (different in development)

--- app/controllers/bitbucket_controller.rb
+++ app/controllers/bitbucket_controller.rb
@@ -4,7 +4,8 @@ class BitbucketController < ApplicationController
 		render :descriptor, locals: { 
 			base_url: 'https://' + ApplicationController.renderer.defaults[:http_host],
 			plugin_key: PluginKeyService::PLUGIN_KEY,
-      client_id: Rails.application.secrets.client_id
+      client_id: Rails.application.secrets.client_id,
+			plugin_name: 'Stars for BitBucket' + (Rails.env.production? ? '' : ' [Development]')
 		}
 	end
 
