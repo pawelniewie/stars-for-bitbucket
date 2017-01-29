f009cebdb618ea94f980c95d0c909c530ae70e48
Pawel Niewiadomski <11110000b@gmail.com>
2017-01-29 21:55:38 +0100

    updated gems

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 ruby '2.3.3'
 source 'https://rubygems.org' do
   # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-  gem 'rails', '>= 5.0.0.rc1', '< 5.1'
+  gem 'rails', '~> 5.0.1', '< 5.1'
   # Use postgresql as the database for Active Record
   gem 'pg', '~> 0.18'
   # Use Puma as the app server
@@ -52,7 +52,7 @@ source 'https://rubygems.org' do
 
   gem 'gibbon'
 
-  gem 'rails-commons', github: 'pawelniewie/rails-commons', branch: 'master'
+  gem 'rails-commons', git: 'https://github.com/pawelniewie/rails-commons.git', branch: 'master'
 
   gem 'shoryuken'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -68,7 +68,8 @@ Rails.application.configure do
     debug: true,
     local: ENV["RAILS_LOG_TO_STDOUT"].present?,
     ssl: true,
-    tag: true
+    tag: true,
+    custom_host: [true, ENV['HEROKU_APP_NAME']]
   )
 
   # Do not dump schema after migrations.
